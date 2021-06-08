fa6c0e6c35e94457264917e956b55dbe5bb52d39
VIvek <gargvivek321@gmail.com>
2021-06-08 20:28:46 +0530

    Constant and Global values added.

---
+++ MedicalPrescription/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  MedicalPrescription
+//
+//  Created by Vivek on 09/06/21.
+//
+
+import Foundation
+
+enum AppConfig {
+    
+    static let apiBaseURL = "https://6082d0095dbd2c001757a8de.mockapi.io/api"
+}
+
+enum Storyboard: String {
+    case main
+    
+    func name() -> String {return rawValue.capitalized}
+}
+
+enum NetworkingError: Error {
+    case domainError
+    case decodingError
+    case connectivityError
+    
+}
+
+enum HttpMethod: String {
+    case get
+    case post
+}
+
+let DEFAULT_PARAMS:[String: String] = [:]
